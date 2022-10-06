4eb7d680acda8114801571c6793658f8313424f3
HajagosNorbert <hajagosnorbi@gmail.com>
2022-10-06 17:09:08 +0200

    before changing inner loop borrow error

--- src/days/day4_1.rs
+++ src/days/day4_1.rs
@@ -1,5 +1,3 @@
-use core::slice::SlicePattern;
-
 pub fn solve(input: &str) -> String {
     //get the generated numbers into a [u8]
     let (generated_nums, boards) = input.split_once('\n').unwrap();
@@ -9,7 +7,7 @@ pub fn solve(input: &str) -> String {
         .collect();
 
     // populate the boards with the rest of the input data. every 25th element is the final cell
-    let boards: Vec<Board> = boards
+    let mut boards: Vec<Board> = boards
         .split("\n\n")
         .map(|table_input| {
             table_input
@@ -25,10 +23,19 @@ pub fn solve(input: &str) -> String {
         .collect();
 
     // go through the generated numbers and change the marked property on each Board's each cell.
-    for gen_num in generated_nums {}
+    let mut solution: u32 = 0;
+    'outer: for num in generated_nums {
+        for board in boards.iter_mut() {
+            let res = board.mark(num);
+            if let Some(answer) = res {
+                solution = answer;
+                break 'outer;
+            };
+        }
+    }
 
-    // println!("{}", boards[0]);
-    boards.last().unwrap().to_string()
+    solution.to_string()
+
     // on each change, check if the rows or columns yield victory by being all marked
 
     // if yes multiply the current generated number with the sum of unmarked values
@@ -39,14 +46,55 @@ pub fn solve(input: &str) -> String {
 
 // create a Board struct with cell-s in it as a 2d matrix
 struct Board {
-    cells: [[Cell; 5]; 5],
+    cells: Vec<Vec<Cell>>,
 }
 
 impl Board {
     fn from(table: &[Vec<u8>]) -> Board {
-        Board {
-            cells: [Cell::from("0")],
+        let cells = table
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .map(|num| Cell::from(*num))
+                    .collect::<Vec<Cell>>()
+            })
+            .collect::<Vec<Vec<Cell>>>();
+        Board { cells }
+    }
+// use indicies instead of iterators
+    fn mark(&mut self, num: u8) -> Option<u32> {
+        for rownum in 0..5 {
+            for colnum in 0..5 {
+                let cell = self.cells.get(rownum).unwrap().get(colnum).unwrap();
+                if cell.value == num && !cell.marked {
+                    let res = self.check_if_bingo(rownum, colnum );
+                    if let Some(answer) = res {
+                        return Some(answer);
+                    }
+
+                }
+            }
         }
+        None
+    }
+
+fn check_if_bingo(&self, rownum: usize, colnum: usize) -> Option<u32> {
+        let mut marked_rows: u8 = 0;
+        let mut marked_cols: u8 = 0;
+        for i in 0..5 {
+            if self.cells.get(rownum).unwrap().get(i).unwrap().marked {
+                marked_rows += 1;
+            };
+            if self.cells.get(i).unwrap().get(colnum).unwrap().marked {
+                marked_cols += 1;
+            };
+        }
+
+        self.cells.get(rownum).unwrap().get(colnum).unwrap().marked = true;
+        if marked_rows == 5 || marked_cols == 5 {
+            return Some(1);
+        }
+        None
     }
 }
 
@@ -54,4 +102,13 @@ impl Board {
 struct Cell {
     value: u8,
     marked: bool,
+}
+
+impl Cell {
+    fn from(num: u8) -> Cell {
+        Cell {
+            value: num,
+            marked: false,
+        }
+    }
 }
